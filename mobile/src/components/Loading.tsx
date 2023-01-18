0988de217ca1fa86dff18acddac0f846af2bd987
Murilo Portugal <murilo.portugal@gmail.com>
2023-01-17 21:35:18 -0300

    Finalizado aula 1

---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import { ActivityIndicator, Text, View } from "react-native";
+
+export function Loading(){
+  return (
+    <View style={{flex:1, justifyContent:'center',alignItems:'center', backgroundColor:'#09090A'}}>
+      <ActivityIndicator color="#7C3AED">
+
+      </ActivityIndicator>
+    </View>
+  )
+}
