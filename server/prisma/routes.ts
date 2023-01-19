b81dd03a88b74761b1e207212c0dc8aea197afae
muriloportugalcasadiconti <murilo.portugal@casadiconti.com.br>
2023-01-19 17:34:00 -0300

    Aula 2 mobile iniciado

--- server/prisma/routes.ts
+++ server/prisma/routes.ts
@@ -38,7 +38,6 @@ export async function appRoutes(app: FastifyInstance){
     const { date } = getDayParams.parse(request.query);
     const parseDate = dayjs(date).startOf('day');
     const weekDay = parseDate.get('day');
-    console.log(weekDay)
 
     const possibleHabits = await prisma.habit.findMany({
       where:{
@@ -53,12 +52,23 @@ export async function appRoutes(app: FastifyInstance){
       }
     });
 
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parseDate.toDate(),
+      },
+      include:{
+        dayHabits: true,
+      }
+    })
+
+    const completedHabits = day?.dayHabits.map(dayHabbit=> dayHabbit.id);
+
     return {
       possibleHabits,
+      completedHabits
     }
-
   });
 
-
+  
 
 }
--- web/src/components/Habit.tsx
+++
@@ -1,9 +0,0 @@
-export function Habit(){
-  return (
-    <div
-      className="bg-zinc-900 w-20 h-10 text-white rounded m-2 flex items-center justify-center"
-    >
-      <p>Habit</p>
-    </div>
-  )
-}
